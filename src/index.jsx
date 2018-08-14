878de252c49feecbf59bfd1cd789f91612cb13b5
brandon <tsabrandon@gmail.com>
2018-08-14 19:05:12 +0800

    2nd commit with first index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+// create a new component.
+//This component should produce some HTML 
+const App = () => {
+    return (
+        <div> 
+            Hi!
+        </div>
+    );
+}
+
+
+
+
+
+
+//Take this component's generated HTML and
+
+//put it on the page in the DOM
